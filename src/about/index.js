5fcfc2a1d429f382fe42c23e235c62e73938a672
BigEyebrowsMan <dbreneshid12@gmail.com>
2024-05-10 10:06:23 +0200

    Seguretat 10:00

---
+++ src/about/index.js
@@ -0,0 +1,16 @@
+import imatge from '/home/dani/Documents/ento-web/src/img/face.png'
+
+function About () {
+    return(
+        <div class="wrapper">
+            <div class="left-side">
+                <div class="vertical-text">Anna Gual</div>
+            </div>
+            
+        </div>
+    )
+
+}
+
+
+export default About;
