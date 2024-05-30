d336421a387ad1521dbdd8e0cbb37ea3d3b1dd4c
unknown <dbreneshid12@gmail.com>
2024-05-30 15:55:08 +0200

    treball fotut2

--- src/nav.js
+++ src/nav.js
@@ -5,7 +5,11 @@ function Navegacio() {
         <nav>
             <ul>
                 <li>
+<<<<<<< HEAD
                     <a href="http://localhost:3000/testing">Main</a>
+=======
+                    <a href="./testing.js">Main</a>
+>>>>>>> aa162b9 (Pre npm)
                 </li>
                 <li>
                     <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Working on it</a>
